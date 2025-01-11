d2d20bf6eb570012d25a41dc499333c668115185
anouther <155570107+anouther@users.noreply.github.com>
2025-01-11 20:33:12 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "Semoga di tahun ini ka cajul bahagia dan semua wishlistnya terwujud ya Kaka cantik ";
+            subtitle.innerText = "Semoga di tahun ini ka cajul bahagia dan semua wishlistnya terwujud ya Kaka cantik🤍🤍 ";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
